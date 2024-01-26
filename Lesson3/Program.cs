ef8a5d3f21cdc70ff1518dd7710cbe38ff15b298
Toffee <toffee@tut.by>
2024-01-26 14:40:43 +0300

    Добавьте файлы проекта.

---
+++ Lesson3/Program.cs
@@ -0,0 +1,122 @@
+﻿#region задание 1
+using System;
+
+Console.WriteLine("Введите номер месяца ввиде числа от 1 до 12:");
+        string month = Console.ReadLine();
+        switch (month)
+        {
+            case "1" or "2" or "12":
+                Console.WriteLine("Пора года - ЗИМА");
+                break;
+            case "3" or "4" or "5":
+                Console.WriteLine("Пора года - ВЕСНА");
+                break;
+            case "6" or "7" or "8":
+                Console.WriteLine("Пора года - ЛЕТО");
+                break;
+            case "9" or "10" or "11":
+                Console.WriteLine("Пора года - ОСЕНЬ");
+                break;
+            default:
+                Console.WriteLine("Вы ввели неизветное значение!");
+                break;
+        }
+#endregion 
+
+#region задание 2
+        Console.WriteLine("Введите номер месяца ввиде числа от 1 до 12:");
+        int month1 = Convert.ToInt32(Console.ReadLine());
+      
+
+        if (month1 == 1 || month1 == 2 || month1 == 12)
+        {
+            Console.WriteLine("Пора года - ЗИМА");
+        }
+        else if (month1 == 3 || month1 == 4 || month1 == 5)
+        {
+            Console.WriteLine("Пора года - ВЕСНА");
+        }
+        else if (month1 == 6 || month1 == 7 || month1 == 8)
+        {
+            Console.WriteLine("Пора года - ЛЕТО");
+        }
+        else if (month1 == 9 || month1 == 10 || month1 == 11)
+        {
+            Console.WriteLine("Пора года - ОСЕНЬ");
+        }
+        else 
+        {
+            Console.WriteLine("Вы ввели неизветное значение!");
+        }
+#endregion 
+
+#region задание 3
+        Console.WriteLine("Введите целое число:");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        if (number % 2 == 0)
+        {
+            Console.WriteLine("Вы ввели четное число");
+        }
+        else
+        {
+            Console.WriteLine("Вы ввели нечетное число");
+        }
+#endregion
+
+
+#region задание 4
+        Console.WriteLine("Введите температуру за окном:");
+        int t = Convert.ToInt32(Console.ReadLine());
+
+        if (t >= -5)
+        {
+            Console.WriteLine("Тепло");
+        }
+        else if (t<-5 && t>=-20)
+        {
+            Console.WriteLine("Нормально");
+        }
+        else
+        {
+            Console.WriteLine("Холодно");
+        }
+#endregion
+#region задание 5
+Console.WriteLine("Введите номер радуги от 1 до 7:");
+string color = Console.ReadLine();
+switch (color)
+{
+    case "1":
+        Console.WriteLine("Красный");
+        break;
+    case "2":
+        Console.WriteLine("Оранжевый");
+        break;
+    case "3":
+        Console.WriteLine("Желтый");
+        break;
+    case "4" :
+        Console.WriteLine("Зеленый");
+        break;
+    case "5":
+        Console.WriteLine("Голубой");
+        break;
+    case "6":
+        Console.WriteLine("Синий");
+        break;
+    case "7":
+        Console.WriteLine("Фиолетовый");
+        break;
+    default:
+        Console.WriteLine("Вы ввели неизветное значение!");
+        break;
+}
+#endregion
+
+
+
+
+
+
+
---
+++ Сalculator/Program.cs
@@ -0,0 +1,105 @@
+﻿using System.ComponentModel.Design;
+using System.Net.Http.Headers;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double a;
+        double b;
+        double result;
+        char oper;
+        while (true)
+        {
+            Console.WriteLine("ВВЕДИТЕ ПЕРВОЕ ЧИСЛО:");
+            if (!Double.TryParse(Console.ReadLine(), out a))
+            { 
+                Console.WriteLine("Ошибка ввода, введите чиcло!"); 
+                Main(args);
+            }
+
+            while (true)
+            {
+                while (true)
+                {
+                    Console.WriteLine("\n Вычитание: - \n Сложение: + \n Умножение: * \n Деление: / \n Процент от числа: % \n Квадратный корень числа: ! \n Результат: = \n ВВЕДИТЕ АРИФМЕТИЧЕСКУЮ ОПЕРАЦИЮ: ");
+                    oper = Convert.ToChar(Console.ReadLine());
+                    if (oper != '-' & oper != '+' & oper != '*' & oper != '/' & oper != '%' & oper != '!' & oper != '=')
+                    {
+                        Console.WriteLine("Ошибка ввода, введите правильно арифметический оператор!");
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                if (oper != '=' & oper != '!')
+                {
+                    while (true)
+                    {
+                        Console.WriteLine("ВВЕДИТЕ СЛЕДУЮЩЕЕ ЧИСЛО:");
+                        if (!Double.TryParse(Console.ReadLine(), out b))
+                        {
+                            Console.WriteLine("Ошибка ввода, введите чиcло!");
+                        }
+                        else 
+                        {
+                            break; 
+                        }
+                    }
+                        //  b = Convert.ToDouble(Console.ReadLine());
+                    if (oper == '-')
+                    {
+                        a = a - b;
+                    }
+                    else if (oper == '+')
+                    {
+                        a = a + b;
+                    }
+                    else if (oper == '/')
+                    {
+                        a = a / b;
+                        if (b == 0)
+                        {
+                            Console.WriteLine("Деление на 0 невозможно");
+                            break;
+                        }
+                    }
+                    else if (oper == '*')
+                    {
+                        a = a * b;
+                    }
+                    else if (oper == '%')
+                    {
+                        a = a / 100 * b;
+                    }
+                }
+                else if (oper == '!')
+                {
+                    if (a > 0)
+                    {
+                        a = Math.Sqrt(a);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Из отрицательного числа {a} нельзя извлечь корень!");
+                        break;
+                    }
+                }
+                else if (oper == '=') 
+                {
+                    result = a;
+                    Console.WriteLine("Результат:" + result);
+                    break;
+                }
+            }
+            Console.WriteLine("\n ПРОДОЛЖИМ? Y:");
+            char Cancel = Convert.ToChar(Console.ReadLine());
+            if (Cancel != 'Y') 
+            {
+                break;
+            }
+        }
+    }
+}
